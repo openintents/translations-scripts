91de60f638e04a53ac62903921643244b3604af7
Peli <peli0101@gmail.com>
2012-05-27 21:15:56 +0200

    add super-scripts to handle git commands for all translatable repositories

---
+++ gitscripts/all_status.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Pull all repositories 
+# May 27, 2012: Peli
+
+# $1..translation file name
+# $2..main path
+function execute
+{
+	translationfilename=$1
+	mainpath=$2
+	gitdir=../../../$mainpath/..
+	echo "Status of $mainpath"
+	git --git-dir="$gitdir/.git" --work-tree="$gitdir" status
+}
+
+# Read all apps that should be translated.
+# sed:
+# - Convert DOS line ending to UNIX line ending using: sed 's/\r//'
+# - Remove comment lines starting with "#"
+# - Remove empty lines
+# apps=( `cat "../applications.txt" | sed -e "s/#.*$//" -e "/^$/d"`)
+apps=( `cat "../applications.txt" | sed -e "s/\r//" -e "s/#.*$//" -e "/^$/d"`)
+
+for (( i = 0 ; i < ${#apps[@]} ; i+=2 ))
+do
+	if [ "${apps[$i]}" == "STOP" ] ; then
+		break
+	fi
+	if [ "${apps[$i]}" == "descriptions" ] ; then
+		continue
+	fi
+	execute ${apps[$i]} ${apps[$i+1]}
+done
+
+# Special cases
+# (a dummy directory is added in the end, because ".." is used above to find the .git repository)
+execute "descriptions" "translations/descriptions/translations"
+execute "export" "translations/export/translations_aboutapp"
+execute "import" "translations/import/translations_aboutapp"
+
